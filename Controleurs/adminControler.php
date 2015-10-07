d1095fd5c1c5513a6cf713af916dfb6e6efbc10c
Grand-Mamy <Grand-Mamy@192.168.1.2>
2015-10-07 21:38:50 +0200

    AdminVue et déconnexion

--- Controleurs/adminControler.php
+++ Controleurs/adminControler.php
@@ -57,7 +57,7 @@ if (isset($_GET['modif']) && ctype_digit($_GET['modif'])) {
 
 
 
-// on a cliqué sur modifier
+// on envoi le form de modif
 if (isset($_GET['modif']) && ctype_digit($_GET['modif']) && isset($_POST['tabUp'])) {
     $idcomment = (int) $_GET['modif'];
     $pour_modif = new CommentClass($_POST['tabUp']);
@@ -66,7 +66,7 @@ if (isset($_GET['modif']) && ctype_digit($_GET['modif']) && isset($_POST['tabUp'
     if ($moi->modifComment($pour_modif, $idcomment)) {
         header("Location: ./");
     } else {
-        
+        header("Location: ./");
     }
 }
 
